9bedb8f55fd568d15d1f29068e06d996053e13e0
Majestr32 <61013205+Majestr32@users.noreply.github.com>
2021-04-01 11:16:49 +0300

    Added dropdown menu with designs, Designed the result page

--- ReactiveSearch/client/src/app/app.module.ts
+++ ReactiveSearch/client/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,6 +21,9 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
     BsDropdownModule.forRoot()
   ],
   providers: [],
